4d422a65d7dba90112810bb3423f5b134bfac74d
Pakhtoon1 <108829433+Pakhtoon1@users.noreply.github.com>
2022-07-20 10:31:11 +0500

    Update M-ULLAH-PUBLIC77.py

--- M-ULLAH-PUBLIC77.py
+++ M-ULLAH-PUBLIC77.py
@@ -49,11 +49,11 @@ ses=requests.Session()
 
 princp=[]
 
-try:
 
-	prox= requests.get('https://api.proxyscrape.com/v2/?request=displayproxies&protocol=socks4&timeout=100000&country=all&ssl=all&anonymity=all').text	open('.prox.txt','w').write(prox)
 
-except Exception as e:
+	
+
+
 
 #Chnge Name 
 
